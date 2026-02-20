6872e9bed71e932f1eee0e46d3a20330adf34125
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2026-02-20 09:08:18 +0100

    move command handlers to a separate module and decompose entry point

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,186 +1,61 @@
 import sys
 from pathlib import Path
 
+from bot.bot_commands import StopCommandsLoop, bot_commands
 from bot.commands import (
-    CommandArgs,
-    CommandContext,
     CommandNotFoundError,
     CommandsDispatcher,
-    CommandsRegistry,
     InvalidCommandArgumentsError,
 )
-from bot.contacts import (
-    ContactAlreadyExistsError,
-    ContactNotFoundError,
-    ContactsBook,
-    ContactsService,
-)
+from bot.contacts import ContactsBook, ContactsService
 
-commands = CommandsRegistry()
+commands_dispatcher = CommandsDispatcher(bot_commands)
 
 
-@commands.register("hello")
-def say_hello() -> None:
-    print("How can I help you?")
-
-
-@commands.register("add", args=["name"], optional_args=["phone number"])
-def add_contact(args: CommandArgs, context: CommandContext) -> None:
-    name, phone = args
-    contacts_service = context["contacts_service"]
-
-    try:
-        match contacts_service.add_contact(name, phone=phone):
-            case "added":
-                print("Contact added.")
-            case "updated:phone":
-                print("Phone number added.")
-    except ContactAlreadyExistsError:
-        print("Contact already exists.")
-
-
-@commands.register("change", args=["name", "old phone number", "new phone number"])
-def change_phone(args: CommandArgs, context: CommandContext) -> None:
-    name, old_phone, new_phone = args
-    contacts_service = context["contacts_service"]
-
-    try:
-        contacts_service.update_contact(name, phone=(old_phone, new_phone))
-        print("Contact updated.")
-    except ContactNotFoundError:
-        print("Contact doesn't exist.")
-
-
-@commands.register("phone", args=["name"])
-def show_phone(args: CommandArgs, context: CommandContext) -> None:
-    name = args[0]
-    contacts_service = context["contacts_service"]
-
-    contact = contacts_service.get_contact(name)
-    if not contact:
-        print("Contact doesn't exist.")
-        return
-
-    if not contact.phones:
-        print("This contact doesn't have a phone number.")
-        return
-
-    print(contact.phones[0])
-
-
-@commands.register("all")
-def show_all(context: CommandContext) -> None:
-    contacts = context["contacts"]
-    if contacts:
-        print(
-            "\n".join(
-                f"{contact.name}: {contact.phones[0] if contact.phones else '-'}"
-                for contact in contacts.values()
-            )
-        )
-    else:
-        print("No contacts.")
-
-
-@commands.register("add-birthday", args=["name", "birthday"])
-def add_birthday(args: CommandArgs, context: CommandContext) -> None:
-    name, birthday = args
-    contacts_service = context["contacts_service"]
-
-    try:
-        match contacts_service.add_birthday(name, birthday=birthday):
-            case "added":
-                print("Birthday added.")
-            case "updated":
-                print("Birthday updated.")
-    except ContactNotFoundError:
-        print("Contact doesn't exist.")
-
-
-@commands.register("show-birthday", args=["name"])
-def show_birthday(args: CommandArgs, context: CommandContext) -> None:
-    name = args[0]
-    contacts_service = context["contacts_service"]
-
-    contact = contacts_service.get_contact(name)
-    if contact:
-        birthday = contact.get_birthday()
-        print(birthday or "Contact doesn't have a birthday set.")
-    else:
-        print("Contact doesn't exist.")
-
-
-@commands.register("birthdays")
-def birthdays(context: CommandContext) -> None:
-    contacts = context["contacts"]
-
-    if not contacts:
-        print("No contacts.")
-        return
-
-    if contacts.birthdays_count == 0:
-        print("No contacts with birthdays.")
-        return
-
-    upcoming_birthdays = contacts.get_upcoming_birthdays()
-    if not upcoming_birthdays:
-        print("No contacts with upcoming birthdays.")
-        return
-
-    print(
-        "\n".join(
-            f"{upcoming_birthday['name']}: {upcoming_birthday['birthday']} (Congratulate: {upcoming_birthday['congratulation_date']})"
-            for upcoming_birthday in upcoming_birthdays
-        )
-    )
-
-
-class StopCommandsLoop(Exception):
-    pass
-
-
-@commands.register("exit", "close", "quit", "bye")
-def say_goodbye() -> None:
-    print("Good bye!")
-    raise StopCommandsLoop
-
-
-dispatcher = CommandsDispatcher(commands)
-
-
-def main() -> None:
-    # Parse CLI arguments to extract contacts book path
-    args = sys.argv[1:]
+def parse_args(args: list[str]) -> Path:
     if len(args) > 1:
         print(f"Usage: python -m bot [path_to_contacts_book]")
         print(f"Expected 0 or 1 arguments, got {len(args)}")
         sys.exit(1)
-    elif len(args) == 1:
-        path = Path(args[0])
-    else:
-        path = None
 
-    # Load contacts book from a file or create a new one
-    if path:
-        try:
-            contacts = ContactsBook.from_file(path)
-        except IsADirectoryError:
-            print(f"File is expected, not a directory: '{path.name}'")
-            sys.exit(1)
-    else:
-        contacts = ContactsBook()
+    return Path(args[0]) if args else Path("contacts.pkl")
 
+
+def load_contacts(path: Path) -> ContactsBook:
+    try:
+        return ContactsBook.from_file(path)
+    except IsADirectoryError:
+        print(f"File is expected, not a directory: '{path.name}'")
+        sys.exit(1)
+
+
+def handle_invalid_command_args_error(error: InvalidCommandArgumentsError) -> None:
+    if error.required_args and error.optional_args:
+        print(
+            f"Give me {error.required_args_str} please, and optionally {error.optional_args_str}."
+        )
+    elif error.required_args:
+        print(f"Give me {error.required_args_str} pleaserror.")
+    elif error.optional_args:
+        print(f"You can optionally provide {error.optional_args_str}.")
+    else:
+        print("This command doesn't take any arguments.")
+
+
+def main() -> None:
+    contacts_path = parse_args(sys.argv[1:])
+    contacts = load_contacts(contacts_path)
     contacts_service = ContactsService(contacts)
 
     # Run commands in a loop
     print("Welcome to the assistant bot!")
     while True:
-        command, command_args = dispatcher.input_command("Enter a command: ")
+        command, command_args = commands_dispatcher.input_command("Enter a command: ")
         if not command:
             continue
 
         try:
-            dispatcher.run_command(
+            commands_dispatcher.run_command(
                 command,
                 *command_args,
                 contacts=contacts,
@@ -189,16 +64,7 @@ def main() -> None:
         except CommandNotFoundError:
             print("Invalid command.")
         except InvalidCommandArgumentsError as e:
-            if e.required_args and e.optional_args:
-                print(
-                    f"Give me {e.required_args_str} please, and optionally {e.optional_args_str}."
-                )
-            elif e.required_args:
-                print(f"Give me {e.required_args_str} please.")
-            elif e.optional_args:
-                print(f"You can optionally provide {e.optional_args_str}.")
-            else:
-                print("This command doesn't take any arguments.")
+            handle_invalid_command_args_error(e)
         except ValueError as e:
             print(e)
         except StopCommandsLoop:
@@ -206,9 +72,8 @@ def main() -> None:
         except Exception as e:
             print(f"Whoops, an unexpected error occurred: {e}")
 
-    # Save contacts book at the end
-    if path:
-        contacts.save(path)
+    # Save contacts at the end
+    contacts.save(contacts_path)
 
 
 if __name__ == "__main__":
